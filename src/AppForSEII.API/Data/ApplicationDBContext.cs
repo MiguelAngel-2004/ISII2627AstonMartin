f1902d3d300c088de60b2f65f7e21004a8eb4221
Marcossa20 <Marcos.Garcia19@alu.uclm.es>
2026-09-26 19:50:17 +0200

    Añadir clase Pista y diagrama CU-ReservarPista

--- src/AppForSEII.API/Data/ApplicationDBContext.cs
+++ src/AppForSEII.API/Data/ApplicationDBContext.cs
@@ -18,6 +18,8 @@ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options
 
     public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
+    public DbSet<Pista> Pistas { get; set; }
+
 
 
 
